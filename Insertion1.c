6b11222a307aef301e89d68c1ed4088456a5038b
Deepanshudubey2005 <deepanshudubey66@gmail.com>
2025-08-31 00:46:58 +0530

    Program to insert the given elements into an array.

--- Insertion1.c
+++ Insertion1.c
@@ -1,5 +1,29 @@
-# include<stdio.h>
-void main()
+#include <stdio.h>
+int main() 
 {
-    printf("Hello world ");
+    int a[50],n,x,i,pos;
+    printf("Enter the number of elements in array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the element to insert: ");
+    scanf("%d", &x);
+    printf("Enter the position to insert : ");
+    scanf("%d", &pos);
+        for (i = n; i > pos; i--) 
+        {
+            a[i] = a[i-1];
+        }
+        a[pos] = x;
+        n++;
+        printf("Array after insertion:\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+    return 0;
 }
